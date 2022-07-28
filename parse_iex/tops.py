6db324e51ac36a754263bbc3da74d791e0dec119
Amit Goren <animalfarm@mailfence.com>
2022-07-28 15:17:40 +0300

    Introducing: Code Documentation🤯

--- parse_iex/tops.py
+++ parse_iex/tops.py
@@ -7,6 +7,30 @@ The amount of seconds in a nanosecond
 NSEC_TO_SEC = 1 / 1000000000
 
 class QuoteUpdate:
+    """
+    A top-quotes update message
+
+    Attributes
+    ----------
+    symbol : str
+        the stock ticker (e.g. 'AAPL')
+    time : datetime.datetime
+        the time of the update with nanoseconds precision
+    bid_price : float
+        best quoted bid price in USD
+    bid_size : int
+        aggregate quoted best bid size
+    ask_price : float
+        best quoted ask price in USD
+    ask_size : int
+        aggregate quoted best ask size
+    
+    Methods
+    -------
+    from_body(body)
+        Decodes a TOPS Quote Update message into a new `QuoteUpdate`.
+    """
+
     def __init__(
         self,
         symbol: str,
@@ -24,6 +48,14 @@ class QuoteUpdate:
         self.ask_size = ask_size
     
     def from_body(body: bytes):
+        """
+        Decodes a TOPS Quote Update message and returns a new `QuoteUpdate`.
+
+        Parameters
+        ----------
+        body : bytes
+            the raw message sans the Message Type field
+        """
         # Seperate the buffer to fields
         data = struct.unpack('<BQ8sIQQI', body)
 
@@ -36,6 +68,27 @@ class QuoteUpdate:
         return QuoteUpdate(symbol, time, bid_price, bid_size, ask_price, ask_size)
 
 class TradeReport:
+    """
+    A trade report
+
+    Attributes
+    ----------
+    symbol : str
+        the stock ticker (e.g. 'AAPL')
+    time : datetime.datetime
+        the time of the trade with nanoseconds precision
+    price : float
+        the trade execution price in USD
+    size : int
+        the no. of shares
+
+    
+    Methods
+    -------
+    from_body(body)
+        Decodes a TOPS Trade Report message into a new `TradeReport`.
+    """
+
     def __init__(
         self,
         symbol: str,
@@ -49,6 +102,14 @@ class TradeReport:
         self.size = size
     
     def from_body(body: bytes):
+        """
+        Decodes a TOPS Trade Report message and returns a new `TradeReport`.
+
+        Parameters
+        ----------
+        body : bytes
+            the raw message sans the Message Type field
+        """
         # Seperate the buffer to fields
         data = struct.unpack('<BQ8sIQq', body)
         
