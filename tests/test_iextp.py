725d26d755692e1cd4f7ad4d60798896afdf724b
Amit Goren <animalfarm@mailfence.com>
2022-08-06 20:38:55 +0300

    Write an IEX-TP v1 parser

---
+++ tests/test_iextp.py
@@ -0,0 +1,15 @@
+import pytest
+
+from datetime import datetime
+
+from parse_iex import iextp
+
+def test_decode():
+    message = b'\x01\x00\xFF\xFF\x01\x00\x00\x00\x00\x00\x87\x42\x07\x00\x02\x00\x8c\xa6\x21\x00\x00\x00\x00\x00\xca\xc3\x00\x00\x00\x00\x00\x00\xec\x45\xc2\x20\x96\x86\x6d\x14\x01\x00\x69\x02\x00\xBE\xEF'
+    
+    session = iextp.Session()
+    outbound_segment = session.decode_packet(message)
+
+    assert outbound_segment.messages == [b'\x69', b'\xBE\xEF']
+    assert outbound_segment.messages_protocol_id == 65535 # non-existent protocol
+    assert not outbound_segment.past
